e258c4a18e8025bb8e415cdb8471ed52e879505a
Pradeep Bhadani <pbhadani@hotels.com>
2018-05-30 10:28:39 +0100

    fixes#8, remove qubole user from mysql

--- mysql.tf
+++ mysql.tf
@@ -19,10 +19,6 @@ data "vault_generic_secret" "hive_rouser" {
   path = "${local.vault_path}/hive_rouser"
 }
 
-data "vault_generic_secret" "qubole_dbuser" {
-  path = "${local.vault_path}/qubole_dbuser"
-}
-
 resource "mysql_user" "hiverw" {
   user               = "${data.vault_generic_secret.hive_rwuser.data["username"]}"
   plaintext_password = "${data.vault_generic_secret.hive_rwuser.data["password"]}"
@@ -32,12 +28,6 @@ resource "mysql_user" "hiverw" {
 resource "mysql_user" "hivero" {
   user               = "${data.vault_generic_secret.hive_rouser.data["username"]}"
   plaintext_password = "${data.vault_generic_secret.hive_rouser.data["password"]}"
-  host               = "%"
-}
-
-resource "mysql_user" "qubole" {
-  user               = "${data.vault_generic_secret.qubole_dbuser.data["username"]}"
-  plaintext_password = "${data.vault_generic_secret.qubole_dbuser.data["password"]}"
   host               = "%"
 }
 
@@ -51,13 +41,6 @@ resource "mysql_grant" "hiverw" {
 resource "mysql_grant" "hivero" {
   user       = "${mysql_user.hivero.user}"
   host       = "${mysql_user.hivero.host}"
-  database   = "${aws_rds_cluster.apiary_cluster.database_name}"
-  privileges = ["SELECT"]
-}
-
-resource "mysql_grant" "qubole" {
-  user       = "${mysql_user.qubole.user}"
-  host       = "${mysql_user.qubole.host}"
   database   = "${aws_rds_cluster.apiary_cluster.database_name}"
   privileges = ["SELECT"]
 }
