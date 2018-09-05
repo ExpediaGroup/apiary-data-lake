f6c9015358c703eb802321039c4c89940e55263e
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2018-09-05 16:16:10 +0100

    fix exposing pass in logs

--- scripts/mysql-user.sh
+++ scripts/mysql-user.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
-MYSQL_OPTIONS="-h $1 --user=$2 --password=$3"
+DB_NAME=$2
+VAULT_PATH=$3
+TYPE=$4
 
-echo "GRANT $4 ON $5.* TO '$6'@\`%\` IDENTIFIED BY '$7';"|mysql $MYSQL_OPTIONS
+MASTER_USER=$(vault read -field=username ${VAULT_PATH}/db_master_user)
+MASTER_PASSWORD=$(vault read -field=password ${VAULT_PATH}/db_master_user)
+
+MYSQL_OPTIONS="-h $1 --user=$MASTER_USER --password='$MASTER_PASSWORD'"
+
+if [ $TYPE == "RW" ]
+then
+  RW_USER=$(vault read -field=username ${VAULT_PATH}/hive_rwuser)
+  RW_PASSWORD=$(vault read -field=password ${VAULT_PATH}/hive_rwuser)
+  echo "GRANT ALL ON $DB_NAME.* TO '${RW_USER}'@\`%\` IDENTIFIED BY '$RW_PASSWORD';"|mysql $MYSQL_OPTIONS
+
+elif [ $TYPE == "RO" ]
+then
+  RO_USER=$(vault read -field=username ${VAULT_PATH}/hive_rouser)
+  RO_PASSWORD=$(vault read -field=password ${VAULT_PATH}/hive_rouser)
+  echo "GRANT SELECT ON $DB_NAME.* TO '${RO_USER}'@\`%\` IDENTIFIED BY '$RO_PASSWORD';"|mysql $MYSQL_OPTIONS
+
+else
+   echo "No condition met"
+fi
