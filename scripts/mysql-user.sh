676b625bd2eb695f5c8bf0064742848b5659d769
pradeepbhadani <pradeep.bhadani@gmail.com>
2019-06-05 13:47:49 +0100

    Fix aws region (#109)
    * fixes #108, Put region flag in aws command
    
    * update CHANGELOG.md

--- scripts/mysql-user.sh
+++ scripts/mysql-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MYSQL_OPTIONS="-h $MYSQL_HOST --user=$MYSQL_MASTER_USER --password=$MYSQL_MASTER_PASSWORD"
 MYSQL_USER=$(aws secretsmanager get-secret-value --secret-id ${MYSQL_SECRET_ARN}|jq .SecretString -r|jq .username -r)
-MYSQL_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${MYSQL_SECRET_ARN}|jq .SecretString -r|jq .password -r)
+MYSQL_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${MYSQL_SECRET_ARN} --region ${AWS_REGION}|jq .SecretString -r|jq .password -r)
 
 echo "GRANT $MYSQL_PERMISSIONS ON $MYSQL_DB.* TO '$MYSQL_USER'@\`%\` IDENTIFIED BY '$MYSQL_PASSWORD';"|mysql $MYSQL_OPTIONS
