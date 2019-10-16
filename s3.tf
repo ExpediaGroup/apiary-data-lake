a31ce41cd482fd6eb0e72356371b9e9f09b90277
Raj Poluri <rpoluri@expediagroup.com>
2019-10-16 18:42:21 -0500

    add ignore_public_acls

--- s3.tf
+++ s3.tf
@@ -109,6 +109,7 @@ resource "aws_s3_bucket_public_access_block" "apiary_bucket" {
 
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls  = true
 }
 
 resource "aws_s3_bucket_notification" "data_events" {
