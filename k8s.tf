3038d9e03ed6b61f7305bb38d826ecb010eb711b
Raj Poluri <rpoluri@expediagroup.com>
2019-11-05 16:55:45 -0600

    add required environment variables

--- k8s.tf
+++ k8s.tf
@@ -26,19 +26,63 @@ resource "kubernetes_deployment" "apiary_hms_readwrite" {
 
       spec {
         container {
-          image = var.docker_image
+          image = var.hms_docker_image
           name  = "hms-readwrite"
 
           env {
-            name  = "AWS_REGION"
-            value = "${var.aws_region}"
+            name  = "MYSQL_DB_HOST"
+            value = var.external_database_host == "" ? join("", aws_rds_cluster.apiary_cluster.*.endpoint) : var.external_database_host
+          }
+
+          env {
+            name  = "MYSQL_DB_NAME"
+            value = var.apiary_database_name
+          }
+
+          env {
+            name  = "MYSQL_SECRET_ARN"
+            value = data.aws_secretsmanager_secret.db_rw_user.arn
+          }
+
+          env {
+            name  = "HIVE_METASTORE_ACCESS_MODE"
+            value = "readwrite"
           }
 
           env {
             name  = "HADOOP_HEAPSIZE"
-            value = "${var.aws_region}"
+            value = var.hms_rw_heapsize
           }
 
+          env {
+            name  = "AWS_REGION"
+            value = var.aws_region
+          }
+
+          env {
+            name  = "HIVE_DB_NAMES"
+            value = join(",", local.apiary_managed_schema_names_original)
+          }
+
+          env {
+            name  = "INSTANCE_NAME"
+            value = local.instance_alias
+          }
+
+          env {
+            name  = "SNS_ARN"
+            value = var.enable_metadata_events == "" ? "" : join("", aws_sns_topic.apiary_metadata_events.*.arn)
+          }
+
+          env {
+            name  = "TABLE_PARAM_FILTER"
+            value = var.enable_metadata_events == "" ? "" : var.table_param_filter
+          }
+
+          env {
+            name  = "ENABLE_METRICS"
+            value = var.enable_hive_metastore_metrics
+          }
 
           resources {
             limits {
@@ -48,6 +92,7 @@ resource "kubernetes_deployment" "apiary_hms_readwrite" {
               memory = "${var.hms_rw_heapsize}Mi"
             }
           }
+
         }
       }
     }
