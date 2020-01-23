fb417d2b793b940a8f20c87597f90f1e6932db2c
rpoluri <38321430+rpoluri@users.noreply.github.com>
2020-01-23 17:28:14 -0600

    Update mysql.tf

--- mysql.tf
+++ mysql.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_config_map" "mysql_datasource" {
   metadata {
-    name      = "mysql-datasource"
+    name      = "apiary-mysql-datasource"
     namespace = "monitoring"
 
     labels = {
@@ -10,16 +10,17 @@ resource "kubernetes_config_map" "mysql_datasource" {
 
   data = {
     "apiary-mysql-datasource.yaml" = <<EOF
-  - name: MySQL
-    type: mysql
-    url: "${var.external_database_host == "" ? join("", aws_rds_cluster.apiary_cluster.*.reader_endpoint) : var.external_database_host}:3306"
-    database: "${var.apiary_database_name}"
-    user: "${data.external.db_ro_user.result["username"]}"
-    password: "${data.external.db_ro_user.result["password"]}"
-    jsonData:
-      maxOpenConns: 0
-      maxIdleConns: 2
-      connMaxLifetime: 14400
+datasources:
+- name: MySQL
+  type: mysql
+  url: "${var.external_database_host == "" ? join("", aws_rds_cluster.apiary_cluster.*.reader_endpoint) : var.external_database_host}:3306"
+  database: "${var.apiary_database_name}"
+  user: "${data.external.db_ro_user.result["username"]}"
+  password: "${data.external.db_ro_user.result["password"]}"
+  jsonData:
+    maxOpenConns: 0
+    maxIdleConns: 2
+    connMaxLifetime: 14400
 EOF
   }
 }
