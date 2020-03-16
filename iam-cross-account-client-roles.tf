331c02adbbcfc987657b2461a8f37dfa07a45786
Scott Barnhart <github@barnharts4.com>
2020-03-16 10:04:57 -0700

    Optionally allow assume_role cross-region access to schema buckets. (#142)
    * Allow cross-region bucket access for assumed roles.
    
    * cleanup
    
    * bug fixes
    
    * readme cleanup
    
    * Apply suggestions from code review
    
    Co-Authored-By: Adrian Woodhead <awoodhead@expediagroup.com>
    
    * PR comments
    
    * fix CHANGELOG
    
    * fix CHANGELOG
    
    * fix CHANGELOG
    
    * update major version number for backwards-incompatible change
    
    Co-authored-by: Scott Barnhart <sbarnhart@expedia.com>
    Co-authored-by: Adrian Woodhead <awoodhead@expediagroup.com>

--- iam-cross-account-client-roles.tf
+++ iam-cross-account-client-roles.tf
@@ -7,7 +7,7 @@
 resource "aws_iam_role" "apiary_assume_role" {
   count                = length(var.apiary_assume_roles)
   name                 = "${local.instance_alias}-${var.apiary_assume_roles[count.index].name}-${var.aws_region}"
-  max_session_duration = lookup(var.apiary_assume_roles[count.index], "max_session_duration", "3600")
+  max_session_duration = lookup(var.apiary_assume_roles[count.index], "max_role_session_duration_seconds", "3600")
 
   assume_role_policy = <<EOF
 {
@@ -45,8 +45,8 @@ resource "aws_iam_role_policy" "apiary_assume_role_s3" {
           "s3:DeleteObject"
       ],
       "Resource": [
-        "${join("\",\"", [for schema in var.apiary_assume_roles[count.index].schema_names : "arn:aws:s3:::${local.apiary_bucket_prefix}-${replace(schema, "_", "-")}"])}",
-        "${join("\",\"", [for schema in var.apiary_assume_roles[count.index].schema_names : "arn:aws:s3:::${local.apiary_bucket_prefix}-${replace(schema, "_", "-")}/*"])}"
+        "${join("\",\"", [for schema in var.apiary_assume_roles[count.index].schema_names : "arn:aws:s3:::${local.apiary_assume_role_bucket_prefix[count.index]}-${replace(schema, "_", "-")}"])}",
+        "${join("\",\"", [for schema in var.apiary_assume_roles[count.index].schema_names : "arn:aws:s3:::${local.apiary_assume_role_bucket_prefix[count.index]}-${replace(schema, "_", "-")}/*"])}"
       ]
     }
   ]
