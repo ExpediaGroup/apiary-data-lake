d1dcc4c17d8923160bdda9de7cf6f48ab4338993
Baskar Jayakumar <bjayakumar@BELC02Q47S2G8WN.sea.corp.expecn.com>
2020-03-16 20:48:36 -0700

    null variable check

--- s3.tf
+++ s3.tf
@@ -48,7 +48,7 @@ resource "aws_s3_bucket" "apiary_data_bucket" {
     }
     expiration {
       days = lookup(var.apiary_managed_schemas[count.index], "s3_bucket_expiry", var.s3_bucket_expiry)
-      count = "${var.s3_bucket_expiry != null ? 1 : 0}"
+      enabled = "${var.s3_bucket_expiry != "null" ? true : false }"
     }
   }
 }
