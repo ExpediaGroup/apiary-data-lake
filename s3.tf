336cc61850317dc9de50b28bb53d296b41b22824
Baskar Jayakumar <bjayakumar@BELC02Q47S2G8WN.sea.corp.expecn.com>
2020-03-18 00:12:23 -0700

    test expiry days

--- s3.tf
+++ s3.tf
@@ -49,9 +49,9 @@ resource "aws_s3_bucket" "apiary_data_bucket" {
     }
 
     dynamic "expiration" {
-    for_each = lookup(var.apiary_managed_schemas[count.index], "s3_bucket_expiry", "") > 0 [] : [1]
+    for_each = length(lookup(var.apiary_managed_schemas, "s3_bucket_expiry", "")) > 0 ? [] : [1]
     content {
-      days = lookup(var.apiary_managed_schemas[count.index], "s3_bucket_expiry", var.s3_bucket_expiry)
+      days = lookup(var.apiary_managed_schemas, "s3_bucket_expiry", null)
       }
     }
   }
