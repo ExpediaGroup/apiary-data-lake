3fb076fd8780bbba9bb49be5012057c1d791e028
sghaisas <sghaisas@expediagroup.com>
2020-03-18 20:11:21 -0700

    added tags in s3.tf for s3 subfolders viz domain,componentinfo

--- s3.tf
+++ s3.tf
@@ -31,7 +31,11 @@ resource "aws_s3_bucket" "apiary_data_bucket" {
   acl           = "private"
   request_payer = "BucketOwner"
   policy        = "${data.template_file.bucket_policy.*.rendered[count.index]}"
-  tags          = "${merge(map("Name", "${element(local.apiary_data_buckets, count.index)}"), "${var.apiary_tags}")}"
+  tags          = "${merge(
+                              map("Name", "${element(local.apiary_data_buckets, count.index)}"),
+                              lookup(element(var.apiary_managed_schemas, count.index), "tags", {}),
+                              "${var.apiary_tags}"
+                              )}"
 
   logging {
     target_bucket = var.apiary_log_bucket == "" ? aws_s3_bucket.apiary_managed_logs_bucket[0].id : var.apiary_log_bucket
