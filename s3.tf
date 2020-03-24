827f1ba18c2e7bc29bf375c2acba8fe9680940a3
Raj Poluri <rpoluri@expediagroup.com>
2020-03-24 10:14:33 -0500

    test schema tag lookup

--- s3.tf
+++ s3.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket" "apiary_data_bucket" {
   request_payer = "BucketOwner"
   policy        = "${data.template_file.bucket_policy.*.rendered[count.index]}"
   //tags          = contains(keys(var.apiary_managed_schemas[count.index]), "tags") ? merge(map("Name", local.apiary_data_buckets[count.index]), var.apiary_tags, var.apiary_managed_schemas[count.index].tags) : merge(map("Name", local.apiary_data_buckets[count.index]), var.apiary_tags)
-  tags = schema_tags[count.index]
+  tags = local.schema_tags[count.index]
 
   logging {
     target_bucket = var.apiary_log_bucket == "" ? aws_s3_bucket.apiary_managed_logs_bucket[0].id : var.apiary_log_bucket
