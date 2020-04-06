6cce60d0b1efb70937dd8c1368bc1246ccd620d0
Scott Barnhart <sbarnhart@expedia.com>
2020-04-06 08:30:01 -0700

    fix version comparison

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -4,11 +4,13 @@ import argparse
 import os
 import re
 import boto3
+from packaging import version
 from datetime import datetime
 try:
     from urlparse import urlparse
 except ImportError:
     from urllib.parse import urlparse
+
 
 class S3Url(object):
     def __init__(self, url):
@@ -37,7 +39,7 @@ def get_terraform_resource_instances(logger, tfstate, resource_type, resource_na
 
 
 def get_schema_map(logger, tfstate):
-    logger.info("Getting Apiary schema names and indices...")
+    logger.info("Getting Apiary schema names and indices:")
 
     bucket_resources = get_terraform_resource_instances(logger, tfstate, 'aws_s3_bucket', 'apiary_data_bucket')
 
@@ -45,29 +47,26 @@ def get_schema_map(logger, tfstate):
     regex = re.compile("apiary.*-\d{12}-[a-zA-Z]+-[a-zA-Z]+-\d-(.+)")
     schema_index_map = { bucket['index_key'] : regex.match(bucket['attributes']['bucket']).group(1) for bucket in bucket_resources }
 
-    if logger.isEnabledFor(logging.DEBUG):
-        for (k,v) in schema_index_map.items():
-            logger.debug(str.format("    Mapped index {} to schema {}", k, v))
+    for (k,v) in schema_index_map.items():
+        logger.info(str.format("    Mapped index {} to schema {}", k, v))
 
     return schema_index_map
 
 
 def change_resource_indices(args, logger, tfstate, resource_type, resource_name, schema_index_map):
-    logger.info(str.format("Updating resource indexes for {}.{}", resource_type, resource_name))
+    logger.info(str.format("Updating resource indexes for {}.{}:", resource_type, resource_name))
     for resource in tfstate['resources']:
         if (resource['name'] == resource_name and resource['type'] == resource_type):
             # Only do following code if count hasn't already been migrated to for_each (each:list --> each:map)
             if (resource['each'] == 'list'):
-                if args.dryrun or logger.isEnabledFor(logging.DEBUG):
-                    logger.debug(str.format('    Changing {0}.{1}.each="list" to {0}.{1}.each="map"', resource_type, resource_name))
+                logger.info(str.format('    Changing {0}.{1}.each="list" to {0}.{1}.each="map"', resource_type, resource_name))
                 if not args.dryrun:
                     resource['each'] = 'map'
 
                 for resource_instance in resource['instances']:
                     index = resource_instance['index_key']
                     schema = schema_index_map[resource_instance['index_key']]
-                    if args.dryrun or logger.isEnabledFor(logging.DEBUG):
-                        logger.debug(str.format('    Changing {0}.{1}[{2}] to {0}.{1}["{3}"]', resource_type, resource_name, index, schema))
+                    logger.info(str.format('    Changing {0}.{1}[{2}] to {0}.{1}["{3}"]', resource_type, resource_name, index, schema))
 
                     if not args.dryrun:
                         resource_instance['index_key'] = schema
@@ -115,14 +114,8 @@ def write_state_to_file(outfile, logger, tfstate):
         logger.error("Unable to write state file: ", exc_info=e)
         exit(1)
 
-def main():
-    # Enable logging
-    logging.basicConfig()
-    logger = logging.getLogger('migrate')
-    logger.setLevel(logging.DEBUG)
 
-    logger.info("Initializing")
-
+def get_args():
     parser = argparse.ArgumentParser(description='Get cmdline args for Apiary Terraform Migrator')
     parser.add_argument('--statefile', help="Terraform Apiary statefile to migrate", action="store")
     parser.add_argument('--outfile', help="Where to write migrated state", action="store")
@@ -135,8 +128,19 @@ def main():
     if not args.outfile:
         args.outfile = args.statefile
 
+    return args
+
+
+def main():
+    # Enable logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-5s: %(message)s', datefmt='%H:%M:%S')
+    logger = logging.getLogger('migrate')
+    logger.info("Initializing")
+
+    args = get_args()
+
     if args.dryrun:
-        logger.info(str.format("-- DRYRUN MODE - No changes will be written to output file {}. --", args.outfile))
+        logger.info(str.format(" ---- DRYRUN MODE - No changes will be written to output file {}. --\n", args.outfile))
 
     logger.info('Loading state file: ' + args.statefile)
     if (args.statefile.startswith('s3://')):
@@ -150,9 +154,15 @@ def main():
         logger.info(str.format("Backing up {} to {}", args.statefile, backupfile))
         write_state_to_file(backupfile, logger, tfstate)
 
-    if tfstate['terraform_version'] < ('0.12.1'):
+    if version.parse(tfstate['terraform_version']) < version.parse('0.12.1'):
         logger.error("This migration script only works on state files created by Terraform 0.12.1+")
         exit(1)
+
+    # Change older version number to one that supports for_each
+    if version.parse(tfstate['terraform_version']) < version.parse('0.12.21'):
+        logger.info(str.format("Changing terraform_version from {} to {}.", tfstate['terraform_version'], '0.12.21'))
+        if not args.dryrun:
+            tfstate['terraform_version'] = '0.12.21'
 
     schema_index_map = get_schema_map(logger, tfstate)
 
@@ -172,6 +182,8 @@ def main():
             write_state_to_file(args.outfile, logger, tfstate)
 
     if args.dryrun:
-        logger.info(str.format("-- DRYRUN MODE - No changes were written to output file {}. --", args.outfile))
+        logger.info(str.format(" ---- DRYRUN MODE - No changes were written to output file {}. --", args.outfile))
+
+
 if __name__ == '__main__':
     main()
