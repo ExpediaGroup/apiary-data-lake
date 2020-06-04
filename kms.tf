3b5c7d60be3ab694c07a917c2514176072b0294b
Raj Poluri <rpoluri@expediagroup.com>
2020-06-03 20:01:16 -0500

    kms keys

---
+++ kms.tf
@@ -0,0 +1,12 @@
+resource "aws_kms_key" "apiary_kms" {
+  for_each = {
+    for schema in local.schemas_info : "${schema["schema_name"]}" => schema
+  }
+
+  description = "apiary ${each.key} kms key"
+  //policy      = "${data.template_file.vault_kms_key_policy.rendered}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
