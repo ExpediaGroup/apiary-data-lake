50b6321839747822d4fb0d3461e588a4162deeef
Anton Kravchuk <akravchuk@expedia.com>
2020-06-12 13:37:23 -0700

    another try to fix the file

--- kms.tf
+++ kms.tf
@@ -1,11 +1,9 @@
-
 resource "aws_kms_key" "apiary_kms" {
   for_each = {
     for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
   }
 
-  description = "apiary ${each.key} kms key"
-
+  description = "Apiary ${each.key} KMS key"
   policy      = data.template_file.apiary_kms_key_policy[each.key].rendered
 
   lifecycle {
