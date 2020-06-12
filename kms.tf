abb02003d9d47e456f37cf9076d481c6c1ffbb82
Anton Kravchuk <akravchuk@expedia.com>
2020-06-12 13:34:57 -0700

    weird issue with updated file

--- kms.tf
+++ kms.tf
@@ -1,9 +1,11 @@
+
 resource "aws_kms_key" "apiary_kms" {
   for_each = {
     for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
   }
 
-  description = "Apiary ${each.key} KMS key"
+  description = "apiary ${each.key} kms key"
+
   policy      = data.template_file.apiary_kms_key_policy[each.key].rendered
 
   lifecycle {
