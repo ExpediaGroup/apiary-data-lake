260f74924c56affc220a6616b524ca14f70d58f4
Anton Kravchuk <akravchuk@expedia.com>
2020-06-15 09:02:08 -0700

    removing KMS keys for testing - do not approve

--- kms.tf
+++ kms.tf
@@ -1,28 +1,28 @@
-resource "aws_kms_key" "apiary_kms" {
-  for_each = {
-    for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
-  }
-
-  description = "Apiary ${each.key} KMS key"
-  policy      = data.template_file.apiary_kms_key_policy[each.key].rendered
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-data "template_file" "apiary_kms_key_policy" {
-  for_each = {
-    for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
-  }
-
-  template = file("${path.module}/templates/apiary-kms-key-policy.json")
-
-  vars = {
-    admin_roles  = replace(each.value["admin_roles"], ",", "\",\"")
-    client_roles = replace(lookup(each.value, "client_roles", ""), ",", "\",\"")
-  }
-}
+//resource "aws_kms_key" "apiary_kms" {
+//  for_each = {
+//    for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
+//  }
+//
+//  description = "Apiary ${each.key} KMS key"
+//  policy      = data.template_file.apiary_kms_key_policy[each.key].rendered
+//
+//  lifecycle {
+//    prevent_destroy = true
+//  }
+//}
+//
+//data "template_file" "apiary_kms_key_policy" {
+//  for_each = {
+//    for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
+//  }
+//
+//  template = file("${path.module}/templates/apiary-kms-key-policy.json")
+//
+//  vars = {
+//    admin_roles  = replace(each.value["admin_roles"], ",", "\",\"")
+//    client_roles = replace(lookup(each.value, "client_roles", ""), ",", "\",\"")
+//  }
+//}
 
 resource "aws_kms_alias" "apiary_alias" {
   for_each = {
