dfe5fd4740bedf4e936c0da46c61bbf9087c4404
Anton Kravchuk <akravchuk@expedia.com>
2020-06-15 09:05:59 -0700

    removing KMS keys for testing - do not approve

--- kms.tf
+++ kms.tf
@@ -23,12 +23,12 @@
 //    client_roles = replace(lookup(each.value, "client_roles", ""), ",", "\",\"")
 //  }
 //}
-
-resource "aws_kms_alias" "apiary_alias" {
-  for_each = {
-    for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
-  }
-
-  name          = "alias/${local.instance_alias}/${each.value["schema_name"]}"
-  target_key_id = aws_kms_key.apiary_kms[each.key].key_id
-}
+//
+//resource "aws_kms_alias" "apiary_alias" {
+//  for_each = {
+//    for schema in local.schemas_info : "${schema["schema_name"]}" => schema if schema["encryption"] == "aws:kms"
+//  }
+//
+//  name          = "alias/${local.instance_alias}/${each.value["schema_name"]}"
+//  target_key_id = aws_kms_key.apiary_kms[each.key].key_id
+//}
