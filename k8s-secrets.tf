50e2134cebc56a63c1f10c9b750dfeda54f024df
Ken Figueiredo <KenFigueiredo@users.noreply.github.com>
2020-08-31 12:12:36 -0400

    [Issue-165] Add init container logic for mysql setup. (#166)
    * Add init container logic for mysql setup.
    
    * add k8s secrets
    
    * rename secret_seed_slug -> secret_name_suffix + EOF line break
    
    * fix syntax errors, make init containers dependent on external_db_host
    
    * use instance alias instead of apiary
    
    * Update date, update description for init_container_image

---
+++ k8s-secrets.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_secret" "hms_secrets" {
+  count = "${var.external_database_host == "" && var.hms_instance_type == "k8s" ? 1 : 0}"
+  metadata {
+    name      = "${local.hms_alias}-credentials"
+    namespace = "metastore"
+  }
+
+  data = {
+    master_creds = aws_secretsmanager_secret_version.apiary_mysql_master_credentials[0].secret_string
+    ro_creds = data.aws_secretsmanager_secret_version.db_ro_user.secret_string
+    rw_creds = data.aws_secretsmanager_secret_version.db_rw_user.secret_string
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
