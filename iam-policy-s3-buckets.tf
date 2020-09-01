c70d968b4a272d8ed80d9c6ed414885d3e160da4
Raj Poluri <rpoluri@expediagroup.com>
2020-09-01 18:34:29 -0500

    use wildcard to configure metastore iam roles

--- iam-policy-s3-buckets.tf
+++ iam-policy-s3-buckets.tf
@@ -30,8 +30,8 @@ resource "aws_iam_role_policy" "s3_data_for_hms_readwrite" {
                               "s3:PutObjectVersionTagging"
                             ],
                   "Resource": [
-                                "${join("\",\"", formatlist("arn:aws:s3:::%s", local.schemas_info[*]["data_bucket"]))}",
-                                "${join("\",\"", formatlist("arn:aws:s3:::%s/*", local.schemas_info[*]["data_bucket"]))}"
+                                "${apiary_bucket_prefix}-*",
+                                "${apiary_bucket_prefix}-*/*"
                               ]
                 }
               ]
@@ -55,8 +55,8 @@ resource "aws_iam_role_policy" "s3_data_for_hms_readonly" {
                               "s3:List*"
                             ],
                   "Resource": [
-                                "${join("\",\"", formatlist("arn:aws:s3:::%s", local.schemas_info[*]["data_bucket"]))}",
-                                "${join("\",\"", formatlist("arn:aws:s3:::%s/*", local.schemas_info[*]["data_bucket"]))}"
+                                "${apiary_bucket_prefix}-*",
+                                "${apiary_bucket_prefix}-*/*"
                               ]
                 }
               ]
@@ -117,156 +117,6 @@ resource "aws_iam_role_policy" "external_s3_data_for_hms_readonly" {
                   "Resource": [
                                 "${join("\",\"", formatlist("arn:aws:s3:::%s", var.external_data_buckets))}",
                                 "${join("\",\"", formatlist("arn:aws:s3:::%s/*", var.external_data_buckets))}"
-                              ]
-                }
-              ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "s3_inventory_for_hms_readwrite" {
-  count = var.s3_enable_inventory ? 1 : 0
-  name  = "s3-inventory"
-  role  = "${aws_iam_role.apiary_hms_readwrite.id}"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-                {
-                  "Effect": "Allow",
-                  "Action": [
-                              "s3:Get*",
-                              "s3:List*"
-                            ],
-                  "Resource": [
-                                "${format("arn:aws:s3:::%s", local.s3_inventory_bucket)}",
-                                "${format("arn:aws:s3:::%s/*", local.s3_inventory_bucket)}"
-                              ]
-                }
-              ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "s3_inventory_for_hms_readonly" {
-  count = var.s3_enable_inventory ? 1 : 0
-  name  = "s3-inventory"
-  role  = "${aws_iam_role.apiary_hms_readonly.id}"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-                {
-                  "Effect": "Allow",
-                  "Action": [
-                              "s3:Get*",
-                              "s3:List*"
-                            ],
-                  "Resource": [
-                                "${format("arn:aws:s3:::%s", local.s3_inventory_bucket)}",
-                                "${format("arn:aws:s3:::%s/*", local.s3_inventory_bucket)}"
-                              ]
-                }
-              ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "s3_access_logs_for_hms_readwrite" {
-  count = local.enable_apiary_s3_log_management ? 1 : 0
-  name  = "s3-access-logs"
-  role  = "${aws_iam_role.apiary_hms_readwrite.id}"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-                {
-                  "Effect": "Allow",
-                  "Action": [
-                              "s3:Get*",
-                              "s3:List*"
-                            ],
-                  "Resource": [
-                                "${format("arn:aws:s3:::%s", local.apiary_s3_hive_logs_bucket)}",
-                                "${format("arn:aws:s3:::%s/*", local.apiary_s3_hive_logs_bucket)}"
-                              ]
-                }
-              ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "s3_access_logs_for_hms_readonly" {
-  count = local.enable_apiary_s3_log_management ? 1 : 0
-  name  = "s3-access-logs"
-  role  = "${aws_iam_role.apiary_hms_readonly.id}"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-                {
-                  "Effect": "Allow",
-                  "Action": [
-                              "s3:Get*",
-                              "s3:List*"
-                            ],
-                  "Resource": [
-                                "${format("arn:aws:s3:::%s", local.apiary_s3_hive_logs_bucket)}",
-                                "${format("arn:aws:s3:::%s/*", local.apiary_s3_hive_logs_bucket)}"
-                              ]
-                }
-              ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "system_for_hms_readwrite" {
-
-  name  = "system"
-  role  = "${aws_iam_role.apiary_hms_readwrite.id}"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-                {
-                  "Effect": "Allow",
-                  "Action": [
-                              "s3:Get*",
-                              "s3:List*"
-                            ],
-                  "Resource": [
-                                "${format("arn:aws:s3:::%s", local.apiary_system_bucket)}",
-                                "${format("arn:aws:s3:::%s/*", local.apiary_system_bucket)}"
-                              ]
-                }
-              ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "system_for_hms_readonly" {
-
-  name  = "system"
-  role  = "${aws_iam_role.apiary_hms_readonly.id}"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-                {
-                  "Effect": "Allow",
-                  "Action": [
-                              "s3:Get*",
-                              "s3:List*"
-                            ],
-                  "Resource": [
-                                "${format("arn:aws:s3:::%s", local.apiary_system_bucket)}",
-                                "${format("arn:aws:s3:::%s/*", local.apiary_system_bucket)}"
                               ]
                 }
               ]
