86cc7c7135070da0c68ca98b886cf25fc537fbd5
Raj Poluri <rpoluri@expediagroup.com>
2020-09-01 20:36:38 -0500

    k8s-secret fix

--- k8s-secrets.tf
+++ k8s-secrets.tf
@@ -7,9 +7,7 @@ resource "kubernetes_secret" "hms_secrets" {
 
   data = {
     master_creds = aws_secretsmanager_secret_version.apiary_mysql_master_credentials[0].secret_string
-    ro_creds = data.aws_secretsmanager_secret_version.db_ro_user.secret_string
-    rw_creds = data.aws_secretsmanager_secret_version.db_rw_user.secret_string
+    ro_creds     = data.aws_secretsmanager_secret_version.db_ro_user.secret_string
+    rw_creds     = data.aws_secretsmanager_secret_version.db_rw_user.secret_string
   }
-
-  type = "kubernetes.io/basic-auth"
 }
