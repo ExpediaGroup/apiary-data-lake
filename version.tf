dff2270c5ede5ef539c16a7e773d7d5767d09499
Raj Poluri <rpoluri@expediagroup.com>
2021-03-01 08:31:42 -0600

    configure required providers

--- version.tf
+++ version.tf
@@ -5,5 +5,11 @@
  */
 
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = "> 0.12.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.0"
+    }
+  }
 }
