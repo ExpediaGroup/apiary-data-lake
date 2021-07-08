24736ece4c2d965118a00334c998950f65feba51
Javier <jabeltran@expediagroup.com>
2021-07-08 15:14:28 +0100

    Change default value

--- variables.tf
+++ variables.tf
@@ -508,5 +508,5 @@ variable "rds_max_allowed_packet" {
 variable "disallow_incompatible_col_type_changes" {
   description = "Hive metastore setting to disallow validation when incompatible schema type changes. Default is false."
   type        = bool
-  default     = false
+  default     = true
 }
