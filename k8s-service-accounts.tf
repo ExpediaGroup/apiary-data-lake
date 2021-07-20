c2f576f9724a68f8271cb4dcfa5c40f2a64adce8
Raj Poluri <rpoluri@expediagroup.com>
2021-07-20 13:11:47 -0500

    fix namespace

--- k8s-service-accounts.tf
+++ k8s-service-accounts.tf
@@ -2,7 +2,7 @@ resource "kubernetes_service_account" "hms_readwrite" {
   count = var.hms_instance_type == "k8s" ? 1 : 0
   metadata {
     name      = "${local.hms_alias}-readwrite"
-    namespace = "metastore"
+    namespace = var.k8s_namespace
   }
   automount_service_account_token = true
 }
@@ -11,7 +11,7 @@ resource "kubernetes_service_account" "hms_readonly" {
   count = var.hms_instance_type == "k8s" ? 1 : 0
   metadata {
     name      = "${local.hms_alias}-readonly"
-    namespace = "metastore"
+    namespace = var.k8s_namespace
   }
   automount_service_account_token = true
 }
