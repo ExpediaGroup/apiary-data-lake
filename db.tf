d48cc41eb6703580eb700ee96b4a0fd4e17401b8
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-02-05 21:06:36 -0800

    Update db.tf

--- db.tf
+++ db.tf
@@ -47,7 +47,7 @@ resource "random_string" "db_master_password" {
 }
 
 resource "aws_rds_cluster_parameter_group" "apiary_rds_param_group" {
-  name_prefix = "${local.instance_alias}-param-group-"
+  name        = "${local.instance_alias}-param-group"
   family      = var.rds_family # Needs to be kept in sync with aws_rds_cluster.apiary_cluster.engine and version
   description = "Apiary-specific Aurora parameters"
   tags        = merge(map("Name", "${local.instance_alias}-param-group"), var.apiary_tags)
@@ -63,8 +63,7 @@ resource "aws_rds_cluster_parameter_group" "apiary_rds_param_group" {
 
 resource "aws_rds_cluster" "apiary_cluster" {
   count                               = var.external_database_host == "" ? 1 : 0
-  cluster_identifier_prefix           = "${local.instance_alias}-cluster-"
-  engine                              = "aurora-mysql"
+  cluster_identifier                  = "${local.instance_alias}-cluster"
   database_name                       = var.apiary_database_name
   master_username                     = var.db_master_username
   master_password                     = random_string.db_master_password[0].result
@@ -86,9 +85,8 @@ resource "aws_rds_cluster" "apiary_cluster" {
 
 resource "aws_rds_cluster_instance" "apiary_cluster_instance" {
   count                                 = var.external_database_host == "" ? var.db_instance_count : 0
-  identifier_prefix                     = "${local.instance_alias}-instance-"
+  identifier                            = "${local.instance_alias}-instance-"
   cluster_identifier                    = aws_rds_cluster.apiary_cluster[0].id
-  engine                                = "aurora-mysql"
   instance_class                        = var.db_instance_class
   db_subnet_group_name                  = aws_db_subnet_group.apiarydbsg[0].name
   publicly_accessible                   = false
