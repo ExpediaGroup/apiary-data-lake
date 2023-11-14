6f6a4edfe6ce7c5e2ce34502b4a99c0e5609dc8b
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-11-14 15:21:42 -0800

    feat: update tag

--- s3-other.tf
+++ s3-other.tf
@@ -217,7 +217,7 @@ resource "aws_s3_bucket_public_access_block" "apiary_access_logs_hive" {
 
 resource "aws_s3_bucket" "apiary_system" {
   bucket = local.apiary_system_bucket
-  tags   = merge(map("Name", local.apiary_system_bucket), var.apiary_tags)
+  tags   = merge(tomap({"Name"=local.apiary_system_bucket}), var.apiary_tags)
   policy = <<EOF
 {
   "Version":"2012-10-17",
