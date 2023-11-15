bf64a3c4725b03775454828c981befc62b536e31
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-11-15 11:43:51 -0800

    feat: fix attribute name

--- ouputs.tf
+++ ouputs.tf
@@ -1,9 +1,9 @@
 output "hms_readonly_load_balancers" {
-  value = var.hms_instance_type == "k8s" && var.enable_vpc_endpoint_services ? kubernetes_service.hms_readonly[0].load_balancer_ingress.*.hostname : []
+  value = var.hms_instance_type == "k8s" && var.enable_vpc_endpoint_services ? kubernetes_service.hms_readonly[0].status.0.load_balancer.0.ingress.0.hostname : []
 }
 
 output "hms_readwrite_load_balancers" {
-  value = var.hms_instance_type == "k8s" && var.enable_vpc_endpoint_services ? kubernetes_service.hms_readwrite[0].load_balancer_ingress.*.hostname : []
+  value = var.hms_instance_type == "k8s" && var.enable_vpc_endpoint_services ? kubernetes_service.hms_readwrite[0].status.0.load_balancer.0.ingress.0.hostname : []
 }
 
 output "managed_database_host" {
