da49cc1ad9564458da6756e9c2d3755de3d3a102
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-11-16 15:41:35 -0800

    feature: remove bucket acl as it's disabled when object_ownership set to `BucketOwnerEnforced` (#242)
    * feat: fix acl
    
    when object_ownership = `BucketOwnerEnforced`, acl is disabled. so need to remove acl.
    
    * feat: fix acl
    
    removed acl as when object_ownership =`BucketOwnerEnforced` ACL is disabled.
    
    * feat: remove bucket ACL

--- s3.tf
+++ s3.tf
@@ -122,14 +122,6 @@ resource "aws_s3_bucket_ownership_controls" "apiary_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "apiary_data_bucket" {
-  for_each = {
-    for schema in local.schemas_info : "${schema["schema_name"]}" => schema
-  }
-  bucket = aws_s3_bucket.apiary_data_bucket[each.key].id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_notification" "data_events" {
   for_each = var.enable_data_events ? {
     for schema in local.schemas_info : "${schema["schema_name"]}" => schema if lookup(schema, "enable_data_events_sqs", "0") == "0"
