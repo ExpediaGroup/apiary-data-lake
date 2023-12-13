1f5b19207cc2c70d54619823f17b6a5d3b324e95
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2023-12-14 02:38:28 +0530

    conflict resolved

--- common.tf
+++ common.tf
@@ -54,6 +54,6 @@ data "aws_secretsmanager_secret_version" "datadog_key" {
 }
 
 provider "datadog" {
-  api_key  = jsondecode("${data.aws_secretsmanager_secret_version.datadog_key.secret_string}").api_key
-  app_key  = jsondecode("${data.aws_secretsmanager_secret_version.datadog_key.secret_string}").app_key
+  api_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).api_key
+  app_key  = jsondecode(data.aws_secretsmanager_secret_version.datadog_key.secret_string).app_key
 }
