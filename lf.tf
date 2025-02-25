285be2eef00e28e1837fff7009f8842118863053
Raj Poluri <rpoluri@expediagroup.com>
2025-02-24 20:04:35 -0600

    fix

--- lf.tf
+++ lf.tf
@@ -5,7 +5,7 @@
  */
 
 resource "aws_lakeformation_resource" "apiary_data_bucket" {
-  for_each              = { for bucket in aws_s3_bucket.apiary_data_bucket : bucket.arn => bucket }
-  arn                   = bucket
+  for_each              = { for bucket in aws_s3_bucket.apiary_data_bucket : bucket => bucket }
+  arn                   = bucket.arn
   hybrid_access_enabled = true
 }
