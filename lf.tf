4be0d742e79e864f8fb27305d676dc60866f3b06
Raj Poluri <rpoluri@expediagroup.com>
2025-02-24 19:57:25 -0600

    create lf resource

---
+++ lf.tf
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2018-2025 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+
+resource "aws_lakeformation_resource" "apiary_data_bucket" {
+  for_each = { for bucket in aws_s3_bucket.apiary_data_bucket : bucket.arn => bucket_arn }
+  arn      = bucket_arn
+}
