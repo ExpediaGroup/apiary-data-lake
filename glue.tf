abc7e625287017b54e633f9c5c896d1050c5172b
Raj Poluri <rpoluri@expediagroup.com>
2025-02-27 15:19:29 -0600

    fix

--- glue.tf
+++ glue.tf
@@ -14,7 +14,7 @@ resource "aws_glue_catalog_database" "apiary_glue_database" {
   description  = "Managed by Apiary terraform"
 }
 
-resource "aws_glue_catalog_database" "apiary_glue_database" {
+resource "aws_glue_catalog_database" "apiary_system_glue_database" {
   count        = var.disable_glue_db_init ? 1 : 0
   location_uri = "s3://${aws_s3_bucket.apiary_system.id}/"
   name         = "${local.gluedb_prefix}${var.system_schema_name}"
