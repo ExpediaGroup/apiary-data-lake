9d78781d707fef935f63f827158551ac1ebfad12
Raj Poluri <rpoluri@expediagroup.com>
2025-02-28 21:29:30 -0600

    remove lf_customer_accounts variable

--- lf.tf
+++ lf.tf
@@ -18,29 +18,3 @@ resource "aws_lakeformation_resource" "apiary_system_bucket" {
 
   hybrid_access_enabled = var.lf_hybrid_access_enabled
 }
-
-locals {
-  lf_schema_customer_accounts = [
-    for pair in setproduct(local.schemas_info[*]["schema_name"], var.lf_customer_accounts) : {
-      schema_name = pair[0]
-      account_id  = pair[1]
-    }
-  ]
-}
-
-#LF policy allowing cross account access
-#to check if we can use IAM groups instead of individual accounts
-# resource "aws_lakeformation_permissions" "glue_db_prems" {
-#   for_each = var.disable_glue_db_init ? tomap({
-#     for schema_account in local.lf_schema_customer_accounts : "${schema_account.schema_name}.${schema_account.account_id}" => schema_account
-#   }) : {}
-#   principal   = each.value.account_id
-#   permissions = ["DESCRIBE"]
-
-#   #required for cross account access, to create resource link in the other account
-#   permissions_with_grant_option = ["DESCRIBE"]
-
-#   database {
-#     name = each.value.schema_name
-#   }
-# }
