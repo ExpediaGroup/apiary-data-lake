3a883de15660dbd68330e089c6462c2fe21e2afc
Raj Poluri <rpoluri@expediagroup.com>
2025-05-05 10:26:02 -0500

    fix TF formatting

--- lf.tf
+++ lf.tf
@@ -107,16 +107,16 @@ locals {
   ]
   # Read accounts
   customer_account_schemas = [
-     for pair in setproduct(local.schemas_info[*]["schema_name"], var.lf_customer_accounts) : {
-       schema_name      = pair[0]
-       customer_account = pair[1]
-     }
-   ]
+    for pair in setproduct(local.schemas_info[*]["schema_name"], var.lf_customer_accounts) : {
+      schema_name      = pair[0]
+      customer_account = pair[1]
+    }
+  ]
   # Write producers
   catalog_producer_schemas = [
     for pair in setproduct(local.schemas_info[*]["schema_name"], var.lf_catalog_producer_arns) : {
-      schema_name = pair[0]
-      producer_arn  = pair[1]
+      schema_name  = pair[0]
+      producer_arn = pair[1]
     }
   ]
 }
@@ -148,59 +148,59 @@ resource "aws_lakeformation_permissions" "catalog_client_system_permissions" {
 }
 
 resource "aws_lakeformation_permissions" "customer_account_permissions" {
-   for_each = var.disable_glue_db_init && var.create_lf_resource ? tomap({
-     for schema in local.customer_account_schemas : "${schema["schema_name"]}-${schema["customer_account"]}" => schema
-   }) : {}
- 
-   principal                     = each.value.customer_account
-   permissions                   = ["DESCRIBE"]
-   permissions_with_grant_option = ["DESCRIBE"]
- 
-   table {
-     database_name = aws_glue_catalog_database.apiary_glue_database[each.value.schema_name].name
-     wildcard      = true
-   }
- }
- 
- resource "aws_lakeformation_permissions" "customer_account_system_permissions" {
-   for_each = var.disable_glue_db_init && var.create_lf_resource ? toset(var.lf_customer_accounts) : []
- 
-   principal                     = each.key
-   permissions                   = ["DESCRIBE"]
-   permissions_with_grant_option = ["DESCRIBE"]
- 
-   table {
-     database_name = aws_glue_catalog_database.apiary_system_glue_database[0].name
-     wildcard      = true
-   }
- }
- 
- resource "aws_lakeformation_permissions" "all_principals_tbl_permissions" {
-   for_each = var.disable_glue_db_init && var.create_lf_resource ? {
-     for schema in local.schemas_info : "${schema["schema_name"]}" => schema
-   } : {}
- 
-   principal   = "${data.aws_caller_identity.current.account_id}:IAMPrincipals"
-   permissions = ["DESCRIBE"]
- 
-   table {
-     database_name = aws_glue_catalog_database.apiary_glue_database[each.key].name
-     wildcard      = true
-   }
- 
- }
- 
- resource "aws_lakeformation_permissions" "all_principals_system_tbl_permissions" {
-   count = var.disable_glue_db_init && var.create_lf_resource ? 1 : 0
- 
-   principal   = "${data.aws_caller_identity.current.account_id}:IAMPrincipals"
-   permissions = ["DESCRIBE"]
- 
-   table {
-     database_name = aws_glue_catalog_database.apiary_system_glue_database[0].name
-     wildcard      = true
-   }
- }
+  for_each = var.disable_glue_db_init && var.create_lf_resource ? tomap({
+    for schema in local.customer_account_schemas : "${schema["schema_name"]}-${schema["customer_account"]}" => schema
+  }) : {}
+
+  principal                     = each.value.customer_account
+  permissions                   = ["DESCRIBE"]
+  permissions_with_grant_option = ["DESCRIBE"]
+
+  table {
+    database_name = aws_glue_catalog_database.apiary_glue_database[each.value.schema_name].name
+    wildcard      = true
+  }
+}
+
+resource "aws_lakeformation_permissions" "customer_account_system_permissions" {
+  for_each = var.disable_glue_db_init && var.create_lf_resource ? toset(var.lf_customer_accounts) : []
+
+  principal                     = each.key
+  permissions                   = ["DESCRIBE"]
+  permissions_with_grant_option = ["DESCRIBE"]
+
+  table {
+    database_name = aws_glue_catalog_database.apiary_system_glue_database[0].name
+    wildcard      = true
+  }
+}
+
+resource "aws_lakeformation_permissions" "all_principals_tbl_permissions" {
+  for_each = var.disable_glue_db_init && var.create_lf_resource ? {
+    for schema in local.schemas_info : "${schema["schema_name"]}" => schema
+  } : {}
+
+  principal   = "${data.aws_caller_identity.current.account_id}:IAMPrincipals"
+  permissions = ["DESCRIBE"]
+
+  table {
+    database_name = aws_glue_catalog_database.apiary_glue_database[each.key].name
+    wildcard      = true
+  }
+
+}
+
+resource "aws_lakeformation_permissions" "all_principals_system_tbl_permissions" {
+  count = var.disable_glue_db_init && var.create_lf_resource ? 1 : 0
+
+  principal   = "${data.aws_caller_identity.current.account_id}:IAMPrincipals"
+  permissions = ["DESCRIBE"]
+
+  table {
+    database_name = aws_glue_catalog_database.apiary_system_glue_database[0].name
+    wildcard      = true
+  }
+}
 
 # Catalog Producer permissions
 
