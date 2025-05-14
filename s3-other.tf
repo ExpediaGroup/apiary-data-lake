990583d524b9fb8009310ac69a294ba876067ba5
janli <janli@expediagroup.com>
2025-05-13 20:41:15 -0700

    fix: add apiary_extra_tags_s3 on inventory and log buckets

--- s3-other.tf
+++ s3-other.tf
@@ -7,7 +7,7 @@
 resource "aws_s3_bucket" "apiary_inventory_bucket" {
   count  = var.s3_enable_inventory == true ? 1 : 0
   bucket = local.s3_inventory_bucket
-  tags   = merge(tomap({ "Name" = "${local.s3_inventory_bucket}" }), var.apiary_tags)
+  tags   = merge(tomap({ "Name" = "${local.s3_inventory_bucket}" }), var.apiary_tags, var.apiary_extra_tags_s3)
 }
 
 resource "aws_s3_bucket_public_access_block" "apiary_inventory_bucket" {
@@ -242,7 +242,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "apiary_managed_logs_bucket" {
 resource "aws_s3_bucket" "apiary_access_logs_hive" {
   count  = local.enable_apiary_s3_log_hive ? 1 : 0
   bucket = local.apiary_s3_hive_logs_bucket
-  tags   = merge(tomap({ "Name" = local.apiary_s3_hive_logs_bucket }), var.apiary_tags)
+  tags   = merge(tomap({ "Name" = local.apiary_s3_hive_logs_bucket }), var.apiary_tags, , var.apiary_extra_tags_s3)
 }
 
 resource "aws_s3_bucket_public_access_block" "apiary_access_logs_hive" {
